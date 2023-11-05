4793e04cbe0b00f5125c4e5ab8fecba5b0ae2f2d
wasim-18972 <wasim-18972@zohocorp.com>
2023-11-05 14:51:11 +0530

    Added local xcframework

--- Package.swift
+++ Package.swift
@@ -9,20 +9,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LocalBinaryTest",
-            targets: ["LocalBinaryTest"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["ZohoDeskPortalAPIKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LocalBinaryTest",
-            dependencies: []),
-        .testTarget(
-            name: "LocalBinaryTestTests",
-            dependencies: ["LocalBinaryTest"]),
+        .binaryTarget(name: "ZohoDeskPortalAPIKit", path: "./Sources/ZohoDeskPortalAPIKit.xcframework")
     ]
 )
--- Sources/LocalBinaryTest/LocalBinaryTest.swift
+++
@@ -1,6 +0,0 @@
-public struct LocalBinaryTest {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
